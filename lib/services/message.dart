76b249d0c4ddc7c2f928375c75dcc8f53f9793ee
lakexyde <lakesida10@gmail.com>
2020-12-19 18:52:49 +0100

    update message service

--- lib/services/message.dart
+++ lib/services/message.dart
@@ -4,6 +4,6 @@ import 'package:wemeet/providers/data.dart';
 
 class MessageService {
   static Future getChats() => api.get("messaging-service/v1/chats", reqToken: DataProvider().messageToken);
-  static Future getConversation(String peerId) => api.get("messaging-service/v1/chats", reqToken: DataProvider().messageToken);
+  static Future getConversation(String peerId) => api.get("messaging-service/v1/$peerId", reqToken: DataProvider().messageToken);
   static Future postLogin() => api.post("messaging-service/v1/login", data:{"userId": DataProvider().user.id.toString()});
 }
