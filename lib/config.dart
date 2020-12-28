cc80a2f5af79d59406770a92d8a99da170916095
TiFe Pariola <tife@pario.la>
2020-12-28 08:48:26 +0100

    update paystack live key

--- lib/config.dart
+++ lib/config.dart
@@ -6,5 +6,5 @@ class WeMeetConfig {
   static const String messageBase = "messaging-service/v1/";
 
   //Paystack public key
-  static const String payStackPublickKey = "pk_test_1ee70468f4f53355ca5b88f3f4d4ac0dd9504749";
+  static const String payStackPublickKey = "pk_live_b747bf32e4fb87b0824a49f3dc4abb831ac64764";
 }
